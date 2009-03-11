6980e87c33eda49bb3d1da65a288695b58264e9d
peschuster <peschuster@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-03-11 23:36:17 +0000

    initial import
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/smilie/trunk@17729 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ class.tx_smilie.php
@@ -0,0 +1,104 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2008-2009 Peter Schuster <typo3@peschuster.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+/**
+ * class.tx_smilie.php
+ *
+ * $Id$
+ *
+ * @author Peter Schuster <typo3@peschuster.de>
+ */
+
+require_once(PATH_t3lib.'class.t3lib_page.php');
+require_once(PATH_t3lib.'class.t3lib_tstemplate.php');
+require_once(PATH_t3lib.'class.t3lib_tsparser_ext.php');
+
+
+/**
+ * Replaces all specific strings in a string with smilie images
+ *
+ * @author		Peter Schuster <typo3@peschuster.de>
+ * @package		TYPO3
+ * @subpackage 	smilie
+ */
+class tx_smilie {
+	var $prefixId		= 'tx_smilie';				// Same as class name
+	var $scriptRelPath	= 'class.tx_smilie.php';	// Path to this script relative to the extension dir.
+	var $extKey			= 'smilie';					// The extension key.
+	var $smilies = array();							// Smilie-Array
+
+/**
+	* Initiates class
+	*
+	* @return	void
+	*/
+	function __construct() {
+		$tsParser = t3lib_div::makeInstance('t3lib_tsparser_ext');
+		$tsParser->tt_track = 0;
+		$tsParser->init();
+		$sys_page = t3lib_div::makeInstance('t3lib_pageSelect');
+		$rootLine = $sys_page->getRootLine(1);
+		$tsParser->runThroughTemplates($rootLine);
+		$tsParser->generateConfig();
+
+		$this->conf = $tsParser->setup['plugin.']['tx_smilie.'];
+
+		$this->arrayParser();
+	}
+
+	/**
+	 * Parses TypoScript-Config-Array and creates and valid smilie array
+	 *
+	 * @return	void
+	 */
+	private function arrayParser() {
+		$this->smilies = array();
+		foreach ($this->conf['smilies.'] as $k => $v) {
+			$this->smilies[$k] = t3lib_div::trimExplode(' ', $v);
+		}
+	}
+
+	/**
+	 * Replaces Smilies with img-HTML-Tag
+	 *
+	 * @param	string		$content: content
+	 * @return	string		HTML
+	 */
+	public function replaceSmilies($content) {
+		$smiliesPath = str_replace('EXT:smilie/',t3lib_extMgm::siteRelPath($this->extKey),$this->conf['smiliePath']);
+
+		foreach ($this->smilies as $path => $smilieArray) {
+			foreach ($smilieArray as $smilie) {
+				$image = '<img alt="'.$smilie.'" title="'.$smilie.'" src="'.$smiliesPath.'/'.$path.'.'.$this->conf['fileExt'].'" />';
+				$content = str_ireplace($smilie, $image, $content);
+			}
+		}
+		return $content;
+	}
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/smilie/class.tx_smilie.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/smilie/class.tx_smilie.php']);
+}
+
+?>
---
+++ ext_emconf.php
@@ -0,0 +1,43 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "smilie"
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Smilie API',
+	'description' => 'Replaces specific strings with smilie images. Completely customizable by TypoScript.',
+	'category' => 'fe',
+	'author' => 'Peter Schuster',
+	'author_email' => 'typo3@peschuster.de',
+	'shy' => '',
+	'dependencies' => 'cms',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'cms' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"d17f";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"fa8c";s:14:"ext_tables.php";s:4:"8872";s:14:"ext_tables.sql";s:4:"eb92";s:35:"icon_tx_simpleshoutbox_messages.gif";s:4:"475a";s:16:"locallang_db.xml";s:4:"8cd3";s:7:"tca.php";s:4:"0cca";s:35:"pi1/class.tx_simpleshoutbox_pi1.php";s:4:"9388";s:17:"pi1/locallang.xml";s:4:"8635";s:24:"pi1/static/editorcfg.txt";s:4:"094d";s:19:"doc/wizard_form.dat";s:4:"3440";s:20:"doc/wizard_form.html";s:4:"7d6a";}',
+);
+
+?>
