bd7693badc02842ad97fb56b29ec23a2495c710c
peschuster <peschuster@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-03-17 11:59:56 +0000

    released version 0.1.2
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/smilie/trunk@17955 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "smilie"
 #
-# Auto generated 12-03-2009 00:50
+# Auto generated 17-03-2009 12:55
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.1.0',
+	'version' => '0.1.1',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:23:{s:9:"ChangeLog";s:4:"d6c2";s:19:"class.tx_smilie.php";s:4:"0cbd";s:12:"ext_icon.gif";s:4:"4cae";s:14:"ext_tables.php";s:4:"3957";s:9:"res/1.gif";s:4:"c8ca";s:10:"res/10.gif";s:4:"fadd";s:10:"res/11.gif";s:4:"3a7a";s:10:"res/12.gif";s:4:"c26c";s:10:"res/13.gif";s:4:"2c1a";s:10:"res/14.gif";s:4:"7fed";s:10:"res/15.gif";s:4:"5d66";s:10:"res/16.gif";s:4:"9b66";s:10:"res/17.gif";s:4:"a39f";s:9:"res/2.gif";s:4:"bc15";s:9:"res/3.gif";s:4:"cc87";s:9:"res/4.gif";s:4:"cbfe";s:9:"res/5.gif";s:4:"b239";s:9:"res/6.gif";s:4:"6432";s:9:"res/7.gif";s:4:"7248";s:9:"res/8.gif";s:4:"ce96";s:9:"res/9.gif";s:4:"db6c";s:16:"static/setup.txt";s:4:"e9d6";s:14:"doc/manual.sxw";s:4:"2fe9";}',
+	'_md5_values_when_last_written' => 'a:24:{s:9:"ChangeLog";s:4:"223c";s:19:"class.tx_smilie.php";s:4:"e26c";s:12:"ext_icon.gif";s:4:"4cae";s:17:"ext_localconf.php";s:4:"84c3";s:14:"ext_tables.php";s:4:"3957";s:9:"res/1.gif";s:4:"c8ca";s:10:"res/10.gif";s:4:"fadd";s:10:"res/11.gif";s:4:"3a7a";s:10:"res/12.gif";s:4:"c26c";s:10:"res/13.gif";s:4:"2c1a";s:10:"res/14.gif";s:4:"7fed";s:10:"res/15.gif";s:4:"5d66";s:10:"res/16.gif";s:4:"9b66";s:10:"res/17.gif";s:4:"a39f";s:9:"res/2.gif";s:4:"bc15";s:9:"res/3.gif";s:4:"cc87";s:9:"res/4.gif";s:4:"cbfe";s:9:"res/5.gif";s:4:"b239";s:9:"res/6.gif";s:4:"6432";s:9:"res/7.gif";s:4:"7248";s:9:"res/8.gif";s:4:"ce96";s:9:"res/9.gif";s:4:"db6c";s:16:"static/setup.txt";s:4:"e9d6";s:14:"doc/manual.sxw";s:4:"b225";}',
 	'suggests' => array(
 	),
 );
